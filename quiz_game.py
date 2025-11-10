429102d69edf78b74a9dd653a000d6309605fc24
RishigheshG <grishighesh@gmail.com>
2025-11-10 18:10:18 +0100

    Game completed!

--- quiz_game.py
+++ quiz_game.py
@@ -1,2 +1,46 @@
 print("Welcome to my quiz game!")
+score = 0
 
+playing = input("Do you want to play? ").lower()
+if playing != "yes":
+  quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ").lower()
+if answer == "central processing unit":
+  print("Correct!")
+  score += 1
+else:
+  print("Incorrect!")
+
+answer = input("What does GPU stand for? ").lower()
+if answer == "graphics processing unit":
+  print("Correct!")
+  score += 1
+else:
+  print("Incorrect!")
+
+answer = input("What does RAM stand for? ").lower()
+if answer == "random access memory":
+  print("Correct!")
+  score += 1
+else:
+  print("Incorrect!")
+
+answer = input("What does PSU stand for? ").lower()
+if answer == "power supply unit":
+  print("Correct!")
+  score += 1
+else:
+  print("Incorrect!")
+
+answer = input("What does FPS stand for? ").lower()
+if answer == "frames per second":
+  print("Correct!")
+  score += 1
+else:
+  print("Incorrect!")
+
+  print("You got " + str(score) + " questions correct!")
+  print("Hope you enjoyed the game!")
